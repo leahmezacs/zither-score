1aac0fd596b27250d92e173cde275de40ff1a02d
Yu Ting Yang <yuting7376@gmail.com>
2019-10-22 13:05:29 -0400

    export NavBar page and import to the home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import NavBar from './NavBar.js';
 
 export default class App extends Component {
     render () {
---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {
+    Navbar,
+    Nav,
+    NavDropdown,
+    Form,
+    FormControl,
+    Button
+    } from 'reactstrap';
+
+
+class NavBar extends Component {
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
