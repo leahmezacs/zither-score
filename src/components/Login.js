377cc4d92359b6181cdae97c57426761435203ab
Mengting Xia <mxia7498@gmail.com>
2019-10-27 19:55:35 -0400

    handle login error

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,3 +4,11 @@ import { Authenticator } from 'aws-amplify-react';
 import { createUser } from '../../mutationHelper';
 import { getUser } from '../../queryHelper';
 import awsExports from '../../aws-exports';
+
+const authErrorMessageMapper = (message) => {
+    if (/incorrect.*username.*password/i.test(message)) {
+        return 'Incorrect username or password';
+    }
+    console.log("ERROR: " + message);
+    return message;
+}
