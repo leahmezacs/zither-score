c6d10de81ad6f4842e134050e390acf9107950f8
Jessica <jchu0398@gmail.com>
2019-11-02 20:49:45 -0400

    Added Library.js page

---
+++ src/components/Library.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+import { Auth } from 'aws-amplify';
+
+class Library extends Component {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Library;
