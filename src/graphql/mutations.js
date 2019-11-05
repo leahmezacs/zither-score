99d67e85600fd078115d854fdfaade61790b0067
Mengting Xia <mxia7498@gmail.com>
2019-11-05 16:27:10 -0500

    user mutations

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,7 +1,27 @@
-export const createUser = `mutation createUser($user: CreateUserInput!) {
-    createUser(input: $user) {
+export const createUser = `mutation createUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+        id
         username
         email
     }
 }`
+
+export const updateUser = `mutation updateUser($input: UpdateUserInput!) {
+    updateUser(input: $input) {
+        id
+        username
+        email
+    }
+}`
+
+export const deleteUser = `mutation deleteUser($user: DeleteUserInput!) {
+    deleteUser(input: $input) {
+        id
+        username
+        email
+        scores
+    }
+}`
+
+
 
