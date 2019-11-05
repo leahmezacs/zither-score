e8865c41b20a1acf19555a04426e7682cc0f1e14
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-11-05 16:30:46 -0500

    blank page

--- src/components/EditScore/EditScore.js
+++ src/components/EditScore/EditScore.js
@@ -8,6 +8,18 @@ class EditScore extends Component {
         return (
             <div>
                 <Container>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                            </Card>
                     
                 </Container>
             </div>
