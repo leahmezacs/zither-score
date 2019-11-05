6557a6421fdd822704117d5fba2b6f1985894861
Jessica <jchu0398@gmail.com>
2019-11-05 10:55:56 -0500

    setting up EditScore.js

---
+++ src/components/EditScore/EditScore.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Auth } from 'aws-amplify';
+import { Container, Row, Col } from 'react-bootstrap/';
+
+
+class EditScore extends Component {
+    render () {
+        return (
+            <div>
+                <Container>
+                    
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default EditScore;
