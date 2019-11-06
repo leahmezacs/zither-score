102f36589648ca2c029f1f4fbea87da4a897acaf
Yu Ting Yang <yuting7376@gmail.com>
2019-11-06 03:20:16 -0500

    add the icon to the toolBar component and install @material-ui/icons module

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Tooltip from '@material-ui/core/Tooltip';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import RemoveIcon from '@material-ui/icons/Remove';
+import DragHandleIcon from '@material-ui/icons/DragHandle';
 
 export default function ToolBar() {
   return (
     <div>
       <Tooltip title="dot" aria-label="dot">
+        <FiberManualRecordIcon fontSize="small"/>
       </Tooltip>
       <Tooltip title="line" aria-label="line">
+        <RemoveIcon />
       </Tooltip>
       <Tooltip title="doule line" aria-label="doubleLine">
+        <DragHandleIcon />
       </Tooltip>
     </div>
   );
