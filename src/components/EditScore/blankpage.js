7c9408092a4ff69cb3fa664a780a78c198d25630
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-11-07 15:58:13 -0500

    setup blank page

---
+++ src/components/EditScore/blankpage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Auth } from 'aws-amplify';
+
+
+class blankpage extends Component {
+    render () {
+        return (
+            <div>
+                <Container>
+                    
+                    
+                </Container>
+            </div>
+        )
+    }
+}
+s
+export default blankpage;
