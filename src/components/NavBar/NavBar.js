fa8b9ad672d83c6146da971a66a3c1aba57f3795
Yu Ting Yang <yuting7376@gmail.com>
2019-11-11 13:05:57 -0500

    change back to react bootstrap styling and add icon to replace icon text

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,52 +1,18 @@
 import React, { Component } from "react";
 import { withRouter, Redirect } from "react-router-dom";
-import { Link } from "react-router-dom";
-import {
-  AppBar,
-  Toolbar,
-  Button,
-  Typography,
-  InputBase,
-  Menu,
-  MenuItem,
-  IconButton
-} from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
-
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      anchorEl: null,
-      open: "",
-    };
-
-    this.setState({
-      open: Boolean(this.state.anchorEl)
-    });
-
-    this.handleMenu = this.handleMenu.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
-
-  handleMenu = event => {
-    this.setState({
-      anchorEl: event.currentTarget,
-      open: Boolean(this.state.anchorEl)
-    });
-  };
-
-  handleClose = () => {
-    this.setState({
-      anchorEl: null
-    });
-  };
-
   render() {
     console.log(this.props.location);
     if (this.props.loggedInUser && this.props.location.pathname === "/Login") {
@@ -55,90 +21,50 @@ class NavBar extends Component {
 
     return (
       <div>
-        <CssBaseline />
-        <AppBar position="static">
-          <Toolbar>
-            <LibraryMusicIcon />
-            {/* <ForumRoundedIcon edge="start" aria-label="UniFree-forum" /> */}
-            <Typography variant="h6">zitherScore</Typography>
-            <Typography variant="h6">Discover</Typography>
-            <div>
-              <div>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
+        <Navbar bg="secondary" expand="lg">
+          <Navbar.Brand href="/Library">Logo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#">Discover</Nav.Link>
+            </Nav>
+
+            <Form inline className="mx-auto">
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
               />
-            </div>
+              <Button variant="outline-warning">Search</Button>
+            </Form>
+
             {this.props.loggedInUser ? (
               <>
-                <Typography variant="h6" component={Link} to="./EditScore">
-                  Create
-                </Typography>
-                <Typography variant="h6">Notification</Typography>
+                <Nav className="ml-auto">
+                  <Nav.Link href="/EditScore">Create</Nav.Link>
+                  <Nav.Link href="#">Notification</Nav.Link>
+                </Nav>
 
-                <div>
-                  <IconButton
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={this.handleMenu}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={this.state.anchorEl}
-                    anchorOrigin={{
-                      vertical: "top",
-                      horizontal: "right"
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right"
-                    }}
-                    open={this.state.open}
-                    onClose={this.handleClose}
-                  >
-                    <MenuItem component={Link} to="#">
-                      Profile
-                    </MenuItem>
-                    <MenuItem component={Link} to="/Library">
-                      Library
-                    </MenuItem>
-                    <MenuItem component={Link} to="/Settings">
-                      Setting
-                    </MenuItem>
-                    <MenuItem component={Link} to="#">
-                      Help
-                    </MenuItem>
-                    <MenuItem
-                      component={Link}
-                      to="/"
-                      onClick={this.props.onSignOut}
-                    >
-                      Logout
-                    </MenuItem>
-                  </Menu>
-                </div>
+                <NavDropdown
+                  title={<AccountCircle color="disabled" fontSize="large" />}
+                  id="basic-nav-dropdown"
+                  className="dropdown-toggle"
+                >
+                  <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+                  <NavDropdown.Item href="/Library">Library</NavDropdown.Item>
+                  <NavDropdown.Item href="/Settings">Settings</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#">Help</NavDropdown.Item>
+                  <NavDropdown.Item href="/" onClick={this.props.onSignOut}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
               </>
             ) : (
-              <>
-                <Button
-                  data-testid="login-button"
-                  variant="contained"
-                  component={Link}
-                  to="/Login"
-                >
-                  Login
-                </Button>
-              </>
+              <Nav.Link href="/Login">Login</Nav.Link>
             )}
-          </Toolbar>
-        </AppBar>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
