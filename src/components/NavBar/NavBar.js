92b2877b84e046a755514041b157d99b608539c9
Yu Ting Yang <yuting7376@gmail.com>
2019-11-11 12:26:00 -0500

    try changing material-ui for better styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,61 +1,147 @@
-import React, {Component} from 'react';
-import { withRouter, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
-    Navbar,
-    Nav,
-    NavDropdown,
-    Form,
-    FormControl,
-    Button
-    } from 'react-bootstrap';
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  InputBase,
+  Menu,
+  MenuItem,
+  IconButton
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 
 class NavBar extends Component {
-    render(){  
-        console.log(this.props.location);
-        if (this.props.loggedInUser && this.props.location.pathname === '/Login') {
-            return <Redirect to="/" />;
-        }
-        
-        return (
-            <div>
-                <Navbar bg="secondary" expand="lg">
-                
-                <Navbar.Brand href="/Library">Logo</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#">Discover</Nav.Link>
-                    </Nav>
+  constructor(props) {
+    super(props);
 
-                    <Form inline className="mx-auto">
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-warning">Search</Button>
-                    </Form>
-                    
-                    {this.props.loggedInUser
-                        ? <>
-                            <Nav className="ml-auto">
-                                <Nav.Link href="/EditScore">Create</Nav.Link>
-                                <Nav.Link href="#">Notification</Nav.Link>
-                            </Nav>
-                            <NavDropdown title="Icon" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#">Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="/Library">Library</NavDropdown.Item>
-                                <NavDropdown.Item href="/Settings">Settings</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#">Help</NavDropdown.Item>
-                                <NavDropdown.Item href="/" onClick={this.props.onSignOut}>Logout</NavDropdown.Item>
-                            </NavDropdown> 
-                        </>
-                        : <Nav.Link href="/Login">Login</Nav.Link>
-                    }
-                    
-                </Navbar.Collapse>
-                </Navbar>
-            </div>
-        )
+    this.state = {
+      anchorEl: null,
+      open: "",
+    };
+
+    this.setState({
+      open: Boolean(this.state.anchorEl)
+    });
+
+    this.handleMenu = this.handleMenu.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleMenu = event => {
+    this.setState({
+      anchorEl: event.currentTarget,
+      open: Boolean(this.state.anchorEl)
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null
+    });
+  };
+
+  render() {
+    console.log(this.props.location);
+    if (this.props.loggedInUser && this.props.location.pathname === "/Login") {
+      return <Redirect to="/" />;
     }
+
+    return (
+      <div>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <LibraryMusicIcon />
+            {/* <ForumRoundedIcon edge="start" aria-label="UniFree-forum" /> */}
+            <Typography variant="h6">zitherScore</Typography>
+            <Typography variant="h6">Discover</Typography>
+            <div>
+              <div>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+            {this.props.loggedInUser ? (
+              <>
+                <Typography variant="h6" component={Link} to="./EditScore">
+                  Create
+                </Typography>
+                <Typography variant="h6">Notification</Typography>
+
+                <div>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={this.handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={this.state.anchorEl}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right"
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right"
+                    }}
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                  >
+                    <MenuItem component={Link} to="#">
+                      Profile
+                    </MenuItem>
+                    <MenuItem component={Link} to="/Library">
+                      Library
+                    </MenuItem>
+                    <MenuItem component={Link} to="/Settings">
+                      Setting
+                    </MenuItem>
+                    <MenuItem component={Link} to="#">
+                      Help
+                    </MenuItem>
+                    <MenuItem
+                      component={Link}
+                      to="/"
+                      onClick={this.props.onSignOut}
+                    >
+                      Logout
+                    </MenuItem>
+                  </Menu>
+                </div>
+              </>
+            ) : (
+              <>
+                <Button
+                  data-testid="login-button"
+                  variant="contained"
+                  component={Link}
+                  to="/Login"
+                >
+                  Login
+                </Button>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 export default withRouter(NavBar);
