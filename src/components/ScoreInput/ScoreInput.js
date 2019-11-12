614c411c8a093f5e1c69b1e0307d27a0aa4ca358
Leah Meza <meza.leah.cs@outlook.com>
2019-11-12 18:22:39 -0500

    css instyling for button

--- src/components/ScoreInput/ScoreInput.js
+++ src/components/ScoreInput/ScoreInput.js
@@ -1,3 +1,11 @@
 import React, {Component} from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
 import { InputGroup, ToggleButtonGroup, ToggleButton} from 'react-bootstrap/';
+
+const buttonStyle = {
+    width:'30px',
+    height: '40px',
+    padding: '5px',
+    margin: '1px',
+    position: 'relative'
+};
