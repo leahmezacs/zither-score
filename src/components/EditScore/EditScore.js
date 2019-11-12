58edbbd6582fdfb578bd273d1ab685d8655a4626
Jessica <jchu0398@gmail.com>
2019-11-11 21:53:27 -0500

    added title content inside page

--- src/components/EditScore/EditScore.js
+++ src/components/EditScore/EditScore.js
@@ -13,7 +13,13 @@ class EditScore extends Component {
                 <div id="svg-score-sheet">
                     <div className="score-scrollable">
                         <svg width="1275" height="1650" className="score-sheet">
-
+                            <g className="page-contents" transform="scale(1.25, 1.25) translate(32.126, 32.126)">
+                                <g className="insideMargin" transform="translate(0,0)">
+                                    <g className="page-title">
+                                        <text font-family="Noto Serif TC" font-style="medium" text-anchor="middle" font-size="40" transform="translate(477.874, 80)">ABC</text>
+                                    </g>
+                                </g>
+                            </g>
                         </svg>
                     </div>
                 </div>
