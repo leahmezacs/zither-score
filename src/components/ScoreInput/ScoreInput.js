7fd17bc24b537f69cc9e260b2fbb0a520e1779e6
Leah Meza <meza.leah.cs@outlook.com>
2019-11-12 18:23:11 -0500

    css instyling for input text

--- src/components/ScoreInput/ScoreInput.js
+++ src/components/ScoreInput/ScoreInput.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
 import { InputGroup, ToggleButtonGroup, ToggleButton} from 'react-bootstrap/';
 
+const inputStyle = {
+    fontSize: '12px',
+    width: '15px',
+    height: '20px',
+    position: 'absolute center'
+};
+
 const buttonStyle = {
     width:'30px',
     height: '40px',
