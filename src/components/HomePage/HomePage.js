3f711cdf742daaa00b2c60936789724cb6554661
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-11-12 14:56:21 -0500

    add home page

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "./HomePage.css";
+
+class HomePage extends Component{
+    render () {
+        return (
+            <div className="MainCard">
+                <div className="title">
+                    <p>Welcome to NumScore!</p>
+                </div>
+            </div>
+        )  
+    }
+}
+
+export default HomePage;
