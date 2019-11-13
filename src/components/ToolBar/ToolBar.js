c4a4ed7d3a7ecbe6e61383fca86e0a86f6eddaaa
Yu Ting Yang <yuting7376@gmail.com>
2019-11-13 02:42:16 -0500

    keep all styling in one css file and remove unused css file

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -5,6 +5,7 @@ import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import RemoveIcon from "@material-ui/icons/Remove";
 import DragHandleIcon from "@material-ui/icons/DragHandle";
 import Grid from "@material-ui/core/Grid";
+import "../style.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,19 +20,19 @@ const useStyles = makeStyles(theme => ({
 class ToolBar extends Component {
   render() {
     return (
-      <div className="toolBarBackground">
+      <div className="toolBarbackground">
         <Grid container justify="center">
           <Grid item>
-            <Tooltip title="dot" aria-label="dot" className="space">
+            <Tooltip title="dot" aria-label="dot" className="toolBarspace">
               <FiberManualRecordIcon fontSize="small" />
             </Tooltip>
-            <Tooltip title="line" aria-label="line" className="space">
+            <Tooltip title="line" aria-label="line" className="toolBarspace">
               <RemoveIcon />
             </Tooltip>
             <Tooltip
               title="doule line"
               aria-label="doubleLine"
-              className="space"
+              className="toolBarspace"
             >
               <DragHandleIcon />
             </Tooltip>
