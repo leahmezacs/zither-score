788009ac79a5df098f7a36accf57ce00b576f5a9
Yu Ting Yang <yuting7376@gmail.com>
2019-11-13 04:44:04 -0500

    fix toolBar bugs and remove unused component from it

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -1,6 +1,4 @@
 import React, {Component} from "react";
-import Tooltip from "@material-ui/core/Tooltip";
-// import Button from "@material-ui/core/Button";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import RemoveIcon from "@material-ui/icons/Remove";
 import DragHandleIcon from "@material-ui/icons/DragHandle";
@@ -13,8 +11,7 @@ class ToolBar extends Component {
     return (
       <div className="toolBarbackground">
         <Grid container justify="center">
-          <Grid item>
-          <Navbar className="" expand="lg">
+          <Navbar expand="lg">
             <NavDropdown title={<FiberManualRecordIcon fontSize="small" />} id="basic-nav-dropdown">
               <NavDropdown.Item>Top</NavDropdown.Item>
               <NavDropdown.Item>Buttom</NavDropdown.Item>
@@ -22,22 +19,6 @@ class ToolBar extends Component {
             <Nav>{<RemoveIcon />}</Nav>
             <Nav>{<DragHandleIcon />}</Nav>
           </Navbar>
-            <Tooltip disableFocusListener title="dot" aria-label="dot" className="toolBarspace">
-              <FiberManualRecordIcon fontSize="small" />
-              {/* <Button>Top</Button>
-              <Button>Buttom</Button> */}
-            </Tooltip>
-            <Tooltip title="line" aria-label="line" className="toolBarspace">
-              <RemoveIcon />
-            </Tooltip>
-            <Tooltip
-              title="doule line"
-              aria-label="doubleLine"
-              className="toolBarspace"
-            >
-              <DragHandleIcon />
-            </Tooltip>
-          </Grid>
         </Grid>
     </div>
     )
