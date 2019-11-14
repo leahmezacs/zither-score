a831123a6ca3bd7582edf32c8bc29df8639490c9
Mengting Xia <mxia7498@gmail.com>
2019-11-14 11:38:23 -0500

    moved stylesheet

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import { withRouter, Redirect } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusicOutlined";
-import "../style.css";
+import "./NavBar.css";
 import PopUpWindow from "../PopUpWindow/PopUpWindow";
 import {
   Navbar,
