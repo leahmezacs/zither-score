1f29c5521786a2c118afd6851cc77b39b8e995c9
Jessica <jchu0398@gmail.com>
2019-11-14 11:57:15 -0500

    reworked library page appearance

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -5,8 +5,8 @@ import { Container, Row, Col } from 'react-bootstrap/';
 class Library extends Component {
     render () {
         return (
-            <div>
-                <Container>
+            <div className="main-library">
+                {/* <Container>
                     <Row>
                         <Col lg xl={2}>
                             <div className="side-bar">
@@ -41,7 +41,43 @@ class Library extends Component {
                             </div>
                         </Col>
                     </Row>
-                </Container>
+                </Container> */}
+                    
+                <div className="side-bar">
+                    <div className="inner">
+                            <a className="list-item" href="#">
+                                <span>Scores</span>
+                            </a>
+                            <div className="sub-item">
+                                <a className="list-item" href="#">
+                                    <span>New collection</span>
+                                </a>
+                            </div>
+                            <a className="list-item" href="#">
+                                <span>Trash</span>
+                            </a>
+                    </div>
+                </div>
+            
+                <div className="main-bar">
+                    <div className="library-content">
+                        <div className="head-container">
+                            <div className="header">
+                                <h1>
+                                    Scores
+                                </h1>
+                            </div>
+                        </div>
+                        
+                        <div className="list-container">
+                            <div className="library-list">
+                                <div className="thead">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
