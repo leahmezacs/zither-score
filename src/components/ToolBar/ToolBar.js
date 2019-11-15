6596f50755bd1ebe06dd5e177ab62bd83e515e24
Jessica <jchu0398@gmail.com>
2019-11-14 21:13:01 -0500

    initial commit

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -1,26 +1,12 @@
 import React, {Component} from "react";
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-import RemoveIcon from "@material-ui/icons/Remove";
-import DragHandleIcon from "@material-ui/icons/DragHandle";
-import Grid from "@material-ui/core/Grid";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import "../../stylesheets/style.css";
+import "../../stylesheets/scorestylesheet.css";
 
 class ToolBar extends Component {
   render() {
     return (
-      <div className="toolBarbackground">
-        <Grid container justify="center">
-          <Navbar expand="lg">
-            <NavDropdown title={<FiberManualRecordIcon fontSize="default" className="colorBlack"/>} id="basic-nav-dropdown">
-              <NavDropdown.Item>Top</NavDropdown.Item>
-              <NavDropdown.Item>Buttom</NavDropdown.Item>
-            </NavDropdown>
-            <Nav className="toolBarspace">{<RemoveIcon />}</Nav>
-            <Nav>{<DragHandleIcon />}</Nav>
-          </Navbar>
-        </Grid>
-    </div>
+      <div>
+          
+      </div>
     )
   }
 }
