b1932472d41b702f9e1ab33c7948cc3fa15d6667
Jessica <jchu0398@gmail.com>
2019-11-15 01:17:37 -0500

    toolbar redesign

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -1,11 +1,32 @@
 import React, {Component} from "react";
-import "../../stylesheets/scorestylesheet.css";
+import "../../stylesheets/style.css";
 
 class ToolBar extends Component {
   render() {
     return (
-      <div>
-          
+      <div className="score-toolbar reg">
+        <div className="score-toolbar-inner">
+          <div className="score-toolbar-main">
+            <div className="score-toolbar-main-tabs">
+              <button id="toolbar-mode-note" className>Note</button>
+              {/* <button id="toolbar-mode-articulation" className>Articulation</button>
+              <button id="toolbar-mode-ornament">Ornament</button>
+              <button id="toolbar-mode-measure">Measure</button> */}
+            </div>
+            <div className="score-toolbar-doc">
+              <div className="toolbar-group">
+                
+              </div>
+            </div>
+          </div>
+          <div className="score-toolbar-div"></div>
+          <div className="score-toolbar-sub">
+            <button className="toolbar-icon mr-2 ml-3">Dot</button>
+            <button className="toolbar-icon mr-2 ml-2">Double Dot</button>
+            <button className="toolbar-icon mr-2 ml-2">Line</button>
+            <button className="toolbar-icon mr-2 ml-3">Double Line</button>
+          </div>
+        </div>
       </div>
     )
   }
