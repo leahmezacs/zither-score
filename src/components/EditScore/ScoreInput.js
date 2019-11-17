76a0de7a792e16e84cf6b5112a196da92c464aa1
Yu Ting Yang <yuting7376@gmail.com>
2019-11-17 08:20:39 -0500

    clean up the file and removed any unnessary comments

--- src/components/EditScore/ScoreInput.js
+++ src/components/EditScore/ScoreInput.js
@@ -1,44 +1,42 @@
 import React, { Component } from "react";
-import SingleLineScoreInput from "./SingleLineScoreInput"
+import SingleLineScoreInput from "./SingleLineScoreInput";
 import Container from "@material-ui/core/Container";
 import ControlPointIcon from "@material-ui/icons/ControlPoint";
 
 class ScoreInput extends Component {
-    constructor(props) {
-        super(props);
-        // const lineLength = []
-        this.state = {
-            line: 0,
-            lineLength: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      line: 0,
+      lineLength: []
+    };
 
-        this.handleNewLine = this.handleNewLine.bind(this);
-    }
+    this.handleNewLine = this.handleNewLine.bind(this);
+  }
 
-    handleNewLine = (event) =>{
-        event.preventDefault();
-        this.setState((preState) => ({
-            line: this.state.line + 1,
-            lineLength: [...preState.lineLength, this.state.line]
-        }))
-    }
+  handleNewLine = event => {
+    event.preventDefault();
+    this.setState(preState => ({
+      line: this.state.line + 1,
+      lineLength: [...preState.lineLength, this.state.line]
+    }));
+  };
 
-    render() {
-        return (
-            <Container maxWidth="lg">
-                <button onClick={this.handleNewLine}>
-                    <ControlPointIcon />
-                </button>
-                <form>
-                    {/* {this.state.lineLength.map((line) => ( */}
-                        <SingleLineScoreInput lineLength={this.state.lineLength} key={this.state.line} />
-                {/* ))} */}
-                    {/* <SingleLineScoreInput /> */}
-                </form>
-            </Container>
-        )
-    }
-
+  render() {
+    return (
+      <Container maxWidth="lg">
+        <button onClick={this.handleNewLine}>
+          <ControlPointIcon />
+        </button>
+        <form>
+          <SingleLineScoreInput
+            lineLength={this.state.lineLength}
+            key={this.state.line}
+          />
+        </form>
+      </Container>
+    );
+  }
 }
 
 export default ScoreInput;
--- src/components/EditScore/SingleLineScoreInput.js
+++ src/components/EditScore/SingleLineScoreInput.js
@@ -3,40 +3,26 @@ import SingleScoreInput from "./SingleScoreInput";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 
-// class SingleLineScoreInput extends Component {
-const SingleLineScoreInput = (props) => {
-  // constructor(props) {
-    // super(props);
-    // const nodeLength = [];
-    // for (let i = 0; i < 4; i++) {
-    //   nodeLength.push(i);
-    // }
-    // this.state = {
-    //   nodeLength: [0, 1, 2, 3]
-    // };
-    const nodeLength = [0, 1, 2, 3];
-  // }
-  // render() {
-    return (
-      props.lineLength.map(value => (
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Grid key={value} container justify="center" spacing={2}>
-              <span>||</span>
-              {nodeLength.map(value => (
-                <Grid key={value} item>
-                  <SingleScoreInput />
-                </Grid>
-              ))}
-              <span>|</span>
-            </Grid>
+const SingleLineScoreInput = props => {
+  const nodeLength = [0, 1, 2, 3];
+
+  return props.lineLength.map(value => (
+    <Container maxWidth="lg">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Grid key={value} container justify="center" spacing={2}>
+            <span>||</span>
+            {nodeLength.map(value => (
+              <Grid key={value} item>
+                <SingleScoreInput />
+              </Grid>
+            ))}
+            <span>|</span>
           </Grid>
         </Grid>
-      </Container>
-      ))
-    );
-  // }
-}
+      </Grid>
+    </Container>
+  ));
+};
 
 export default SingleLineScoreInput;
