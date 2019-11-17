0fc74998501bafe9944779d4aceadff0da1d6f90
Yu Ting Yang <yuting7376@gmail.com>
2019-11-17 04:38:05 -0500

    add a template of pass singleScoreInput using map

--- src/components/EditScore/ScoreInputForm.js
+++ src/components/EditScore/ScoreInputForm.js
@@ -1,15 +1,60 @@
 import React, { Component } from "react";
 import Container from '@material-ui/core/Container';
 import SingleScoreInput  from "./SingleScoreInput";
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
 
 class ScoreInputForm extends Component {
   render() {
-    return (
-        <Container maxWidth="xs">
-            <SingleScoreInput />
-        </Container>
+    // return (
+    //     <Container maxWidth="xs">
+            
+    //         <SingleScoreInput />
+    //     </Container>
       
-    );
+    // );
+    return (
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="center" spacing={2}>
+              {[0, 1, 2].map(value => (
+                <Grid key={value} item>
+                  <SingleScoreInput />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper >
+              <Grid container>
+                <Grid item>
+                  <FormLabel>spacing</FormLabel>
+                  <RadioGroup
+                    name="spacing"
+                    aria-label="spacing"
+                    // value={spacing.toString()}
+                    // onChange={handleChange}
+                    row
+                  >
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => (
+                      <FormControlLabel
+                        key={value}
+                        value={value.toString()}
+                        control={<Radio />}
+                        label={value.toString()}
+                      />
+                    ))}
+                  </RadioGroup>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      );
   }
 }
 
