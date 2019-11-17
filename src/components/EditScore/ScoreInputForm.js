c9c344d759dc71946a47c8520d75a00f86aa4679
Yu Ting Yang <yuting7376@gmail.com>
2019-11-17 04:11:08 -0500

    create a new page and import singleScoreInput component to it

---
+++ src/components/EditScore/ScoreInputForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { SingleScoreInput } from "./SingleScoreInput"
+
+class ScoreInputForm extends Component {
+  render() {
+    return (
+      <ScoreInputForm />
+    );
+  }
+}
+
+export default ScoreInputForm;
