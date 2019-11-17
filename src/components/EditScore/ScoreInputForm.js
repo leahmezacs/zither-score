f1743d3e0c9b6fa6aae080be145460290b58a0de
Yu Ting Yang <yuting7376@gmail.com>
2019-11-17 04:59:56 -0500

    add the nodeLength array to map through array for scoreInput

--- src/components/EditScore/ScoreInputForm.js
+++ src/components/EditScore/ScoreInputForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Container from '@material-ui/core/Container';
 import SingleScoreInput  from "./SingleScoreInput";
 import Grid from '@material-ui/core/Grid';
 import FormLabel from '@material-ui/core/FormLabel';
@@ -9,52 +8,63 @@ import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
 
 class ScoreInputForm extends Component {
+    constructor(props) {
+        super(props);
+
+        const nodeLength = [];
+        for (let i = 0; i < 16; i++) {
+            nodeLength.push(i);
+        }
+        this.state = {
+            nodeLength
+        };
+    }
   render() {
-    // return (
-    //     <Container maxWidth="xs">
-            
-    //         <SingleScoreInput />
-    //     </Container>
-      
-    // );
     return (
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Grid container justify="center" spacing={2}>
-              {[0, 1, 2].map(value => (
-                <Grid key={value} item>
-                  <SingleScoreInput />
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper >
-              <Grid container>
-                <Grid item>
-                  <FormLabel>spacing</FormLabel>
-                  <RadioGroup
-                    name="spacing"
-                    aria-label="spacing"
-                    // value={spacing.toString()}
-                    // onChange={handleChange}
-                    row
-                  >
-                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => (
-                      <FormControlLabel
-                        key={value}
-                        value={value.toString()}
-                        control={<Radio />}
-                        label={value.toString()}
-                      />
-                    ))}
-                  </RadioGroup>
-                </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            {this.state.nodeLength.map(value => (
+              <Grid key={value} item>
+                {/* <SingleScoreInput /> */}
+                <input
+                  className="singleNote"
+                  type="number"
+                  maxLength="1"
+                  min="1"
+                  max="7"
+                />
               </Grid>
-            </Paper>
+            ))}
           </Grid>
         </Grid>
-      );
+        <Grid item xs={12}>
+          <Paper>
+            <Grid container>
+              <Grid item>
+                <FormLabel>spacing</FormLabel>
+                <RadioGroup
+                  name="spacing"
+                  aria-label="spacing"
+                  // value={spacing.toString()}
+                  // onChange={handleChange}
+                  row
+                >
+                  {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => (
+                    <FormControlLabel
+                      key={value}
+                      value={value.toString()}
+                      control={<Radio />}
+                      label={value.toString()}
+                    />
+                  ))}
+                </RadioGroup>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
