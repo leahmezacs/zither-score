d2b61a35c973ddcd5e406b4ef0b387da47e9c591
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-11-19 18:29:20 -0500

    add symbols

--- src/components/EditScore/SingleScoreInput.js
+++ src/components/EditScore/SingleScoreInput.js
@@ -4,102 +4,121 @@ import Container from "@material-ui/core/Container";
 import ControlPointIcon from "@material-ui/icons/ControlPoint";
 import Button from "@material-ui/core/Button";
 import { Dropdown } from "react-bootstrap";
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import MinimizeIcon from '@material-ui/icons/Minimize';
+import DragHandleIcon from '@material-ui/icons/DragHandle';
+
+
 
 class SingleScoreInput extends Component {
   render() {
     return (
       <div>
         <div>
+
           <Dropdown>
             <Dropdown.Toggle className="btn btn-sm btn-light">
-              <MoreVertIcon />
+              <MoreHorizIcon
+                fontSize="small"
+              />
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item>dot</Dropdown.Item>
-              <Dropdown.Item>double dot</Dropdown.Item>
-              <Dropdown.Item>line</Dropdown.Item>
-              <Dropdown.Item>double line</Dropdown.Item>
+              <Dropdown.Item><FiberManualRecordIcon /></Dropdown.Item>
+              <Dropdown.Item><MinimizeIcon /></Dropdown.Item>
+              <Dropdown.Item><DragHandleIcon /></Dropdown.Item>
+              <Dropdown.Item>double dots</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
 
+
+          <input
+            key="0"
+            className="singleNote"
+            type="number"
+            min="0"
+            max="7"
+          />
         </div>
-        <input
-          key="0"
-          className="singleNote"
-          type="number"
-          min="0"
-          max="7"
-        />
+
         <div>
           <Dropdown>
             <Dropdown.Toggle className="btn btn-sm btn-light">
-              <MoreVertIcon />
+              <MoreHorizIcon
+                fontSize="small" />
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item>dot</Dropdown.Item>
-              <Dropdown.Item>double dot</Dropdown.Item>
-              <Dropdown.Item>line</Dropdown.Item>
-              <Dropdown.Item>double line</Dropdown.Item>
+              <Dropdown.Item><FiberManualRecordIcon /></Dropdown.Item>
+              <Dropdown.Item><MinimizeIcon /></Dropdown.Item>
+              <Dropdown.Item><DragHandleIcon /></Dropdown.Item>
+              <Dropdown.Item>double dots</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
 
-        </div>
-        <input
-          key="1"
-          className="singleNote"
-          type="number"
-          min="1"
-          max="7"
-        />
 
-        
+          <input
+            key="1"
+            className="singleNote"
+            type="number"
+            min="1"
+            max="7"
+          />
+
+
+        </div>
+
         <div>
           <Dropdown>
             <Dropdown.Toggle className="btn btn-sm btn-light">
-              <MoreVertIcon />
+              <MoreHorizIcon
+                fontSize="small" />
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item>dot</Dropdown.Item>
-              <Dropdown.Item>double dot</Dropdown.Item>
-              <Dropdown.Item>line</Dropdown.Item>
-              <Dropdown.Item>double line</Dropdown.Item>
+              <Dropdown.Item><FiberManualRecordIcon /></Dropdown.Item>
+              <Dropdown.Item><MinimizeIcon /></Dropdown.Item>
+              <Dropdown.Item><DragHandleIcon /></Dropdown.Item>
+              <Dropdown.Item>double dots</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
 
+
+
+
+          <input
+            key="2"
+            className="singleNote"
+            type="number"
+            min="1"
+            max="7"
+          />
         </div>
 
-        
-        <input
-          key="2"
-          className="singleNote"
-          type="number"
-          min="1"
-          max="7"
-        />
         <div>
           <Dropdown>
             <Dropdown.Toggle className="btn btn-sm btn-light">
-              <MoreVertIcon />
+              <MoreHorizIcon
+                fontSize="small" />
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item>dot</Dropdown.Item>
-              <Dropdown.Item>double dot</Dropdown.Item>
-              <Dropdown.Item>line</Dropdown.Item>
-              <Dropdown.Item>double line</Dropdown.Item>
+              <Dropdown.Item><FiberManualRecordIcon /></Dropdown.Item>
+              <Dropdown.Item><MinimizeIcon /></Dropdown.Item>
+              <Dropdown.Item><DragHandleIcon /></Dropdown.Item>
+              <Dropdown.Item>double dots</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
 
+
+
+          <input
+            key="3"
+            className="singleNote"
+            type="number"
+            min="1"
+            max="7"
+          />
+          <span className="lineInBetween">|</span>
         </div>
 
-        <input
-          key="3"
-          className="singleNote"
-          type="number"
-          min="1"
-          max="7"
-        />
-        <span className="lineInBetween">|</span>
       </div>
     );
   }
