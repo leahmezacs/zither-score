42c8b7e487788525c6e6f7070f49e5f21b3466ae
Leah Meza <meza.leah.cs@outlook.com>
2019-11-20 00:21:03 -0500

    snapshot cahnged due to name change

--- src/components/CreateModal/CreateModal.test.js
+++ src/components/CreateModal/CreateModal.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow } from 'enzyme';
+import { Modal } from 'react-bootstrap';
 import CreateModal from './CreateModal'
 
 describe("CreateModal", () => {
-    it('render a createmodal component', () => {  
+    it('render createmodal without crashing', () => {  
         const CreateModalComp = shallow(<div><CreateModal /></div>);
         expect(CreateModalComp).toMatchSnapshot();
     });
+
+    it('renders Modal component', () => {  
+        const CreateModalComp = shallow(<CreateModal />);
+        expect(CreateModalComp.find(CreateModal).length).toEqual(1);  
+    });
+
+
 });
+
+ 
