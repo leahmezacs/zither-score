3380ef1e93337382ee624c5e12a26de572d653fb
Yu Ting Yang <yuting7376@gmail.com>
2019-11-20 11:29:32 -0500

    able to handle individual index with column index

--- src/components/EditScore/SingleScoreInput.js
+++ src/components/EditScore/SingleScoreInput.js
@@ -1,45 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../stylesheets/style.css"
 
 // Four inputs in one componenet
 const SingleScoreInput = props => {
 
-  console.log(props.lineLength)
-  console.log(props.nodeLength)
+  // console.log(props.lineLength)
+  // console.log(props.nodeLength)
+
+  const [data, setData] = useState([]);
+
+  const handleChange = (event) => {
+    const updateData = {...data};
+    updateData[event.target.name] = event.target.value;
+    setData(updateData);
+    console.log(data);
+    // console.log(event.target.name)
+  };
+
 
   return props.nodeLength.map(values => (
     <span key={values}>
+    {/* {console.log(typeof(values))} */}
       <input
         key="0"
-        name="0"
+        name={[0, values]}
         className="singleNote"
         type="number"
         min="0"
         max="7"
+        onChange={handleChange}
       />
       <input
         key="1"
-        name="1"
+        name={[1, values]}
         className="singleNote"
         type="number"
         min="1"
         max="7"
+        onChange={handleChange}
       />
       <input
         key="2"
-        name="2"
+        name={[2, values]}
         className="singleNote"
         type="number"
         min="1"
         max="7"
+        onChange={handleChange}
       />
       <input
         key="3"
-        name="3"
+        name={[3, values]}
         className="singleNote"
         type="number"
         min="1"
         max="7"
+        onChange={handleChange}
       />
       <span className="lineInBetween">|</span>
     </span>
