10490e306477c58a2d0e922433ae2741fb47a0da
Leah Meza <meza.leah.cs@outlook.com>
2019-11-19 23:27:49 -0500

    updated unit test using snapshot

--- src/components/CreateModal/CreateModal.test.js
+++ src/components/CreateModal/CreateModal.test.js
@@ -2,17 +2,10 @@ import React from 'react';
 import {renderer, shallow} from 'enzyme';
 import NavBar from '../NavBar/NavBar'
 import CreateModal from './CreateModal'
-import { Button, Modal} from 'react-bootstrap';
 
 describe("CreateModal", () => {
-    it('renders modal when show is true', () => {
-        const props = { show: true }; 
-        const wrapper = shallow(
-          <Modal.Header closeButton>
-            <Modal.Title id="title">Music File</Modal.Title>
-          </Modal.Header>
-        );
-      
-        expect(wrapper.text()).toContain('Music File');
-      });
+    it('render a createmodal component', () => {  
+        const CreateModalComp = shallow(<div><CreateModal /></div>);
+        expect(CreateModalComp).toMatchSnapshot();
+    });
 });
