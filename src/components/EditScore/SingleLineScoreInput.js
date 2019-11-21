4bb4df35663c6f291b3b2ef44ea7550299333854
Yu Ting Yang <yuting7376@gmail.com>
2019-11-21 13:11:11 -0500

    fixing errors for scoreInput

--- src/components/EditScore/SingleLineScoreInput.js
+++ src/components/EditScore/SingleLineScoreInput.js
@@ -1,41 +1,41 @@
 import React from "react";
 import SingleScoreInput from "./SingleScoreInput";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
 
 // Map through singleInput for one line score input
 const SingleLineScoreInput = props => {
   const nodeLength = [0, 1, 2, 3];
 
-  return props.lineLength.map(values => (
-    <Container className="lineSpacing" key={values} maxWidth="lg">
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify="center" spacing={2}>
-            <span className="lineBegin">|</span>
-            <Grid item>
-                <SingleScoreInput
-                  lineLength={values}
-                  nodeLength={nodeLength}
-                />
-              </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
-  ));
+  return (
+    <SingleScoreInput lineLength={props.lineLength} nodeLength={nodeLength} />
+  );
 };
 
 export default SingleLineScoreInput;
 
 
-// {nodeLength.map(value => (
-//   <Grid key={value} item>
-//     <SingleScoreInput
-//       key={value}
-//       lineLength={values}
-//       nodeLength={value}
-//     />
-//     {/* {console.log(values)} */}
-//   </Grid>
-// ))}
+
+// import React from "react";
+// import SingleScoreInput from "./SingleScoreInput";
+// import Container from "@material-ui/core/Container";
+// import Grid from "@material-ui/core/Grid";
+// // Map through singleInput for one line score input
+// const SingleLineScoreInput = props => {
+//   const nodeLength = [0, 1, 2, 3];
+
+//   return props.lineLength.map(row => (
+//     <Container className="lineSpacing" key={row} maxWidth="lg">
+//       <Grid container spacing={2}>
+//         <Grid item xs={12}>
+//           <Grid container justify="center" spacing={2}>
+//             <span className="lineBegin">|</span>
+//             <Grid item>
+//               <SingleScoreInput lineLength={row} nodeLength={nodeLength} />
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </Grid>
+//     </Container>
+//   ));
+// };
+
+// export default SingleLineScoreInput;
