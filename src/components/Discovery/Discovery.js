2a2316953c950718145fb62b5e3976d16cbddc54
Yu Ting Yang <yuting7376@gmail.com>
2019-11-26 18:48:18 -0500

    modify material-ui/core version and change import icon to html stylesheet

--- src/components/Discovery/Discovery.js
+++ src/components/Discovery/Discovery.js
@@ -115,42 +115,11 @@
 
 // export default Discovery;
 import React, { Component } from 'react';
-import MaterialTable from 'material-table';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import { Auth, graphqlOperation, API } from "aws-amplify";
+import * as queries from "../../graphql/queries";
 
-const tableIcons = {
-  Add: AddBox,
-  Check: Check,
-  Clear: Clear,
-  Delete: DeleteOutline,
-  DetailPanel: ChevronRight,
-  Edit: Edit,
-  Export: SaveAlt,
-  Filter: FilterList,
-  FirstPage: FirstPage,
-  LastPage: LastPage,
-  NextPage: ChevronRight,
-  PreviousPage: ChevronLeft,
-  ResetSearch: Clear,
-  SortArrow: ArrowUpward,
-  ThirdStateCheck: Remove,
-  Search: Search,
-  ViewColumn: ViewColumn,
-  };
+import MaterialTable from 'material-table';
+import Container from '@material-ui/core/Container';
 
 class Discovery extends Component {
     constructor(props){
@@ -169,51 +138,68 @@ class Discovery extends Component {
       }
     }
 
+  //get public scores from all users
+  // async componentDidMount() {
+  //   const result = await API.graphql(graphqlOperation(queries.listNotes, { 
+  //     limit: 200, 
+  //     filter: {
+  //       status: 'PUBLIC'
+  //     }})); 
+  //   console.log(result);
+  //   this.setState({
+  //     scores: result.data.listScores.items,
+  //   });
+  //   console.log(this.state.scores);
+  // }
+
   render(){
   return (
+    <Container maxWidth="md">
+    <br />
     <MaterialTable
-      icons={tableIcons}
-      title="Editable Example"
+      // icons={tableIcons}
+      title="Public Score"
       columns={this.state.columns}
       data={this.state.data}
       // editable={{
-      //   // onRowAdd: newData =>
-      //   //   new Promise(resolve => {
-      //   //     setTimeout(() => {
-      //   //       resolve();
-      //   //       this.setState(prevState => {
-      //   //         const data = [...prevState.data];
-      //   //         data.push(newData);
-      //   //         return { ...prevState, data };
-      //   //       });
-      //   //     }, 600);
-      //   //   }),
-      //   // onRowUpdate: (newData, oldData) =>
-      //   //   new Promise(resolve => {
-      //   //     setTimeout(() => {
-      //   //       resolve();
-      //   //       if (oldData) {
-      //   //         this.setState(prevState => {
-      //   //           const data = [...prevState.data];
-      //   //           data[data.indexOf(oldData)] = newData;
-      //   //           return { ...prevState, data };
-      //   //         });
-      //   //       }
-      //   //     }, 600);
-      //   //   }),
-      //   // onRowDelete: oldData =>
-      //   //   new Promise(resolve => {
-      //   //     setTimeout(() => {
-      //   //       resolve();
-      //   //       this.setState(prevState => {
-      //   //         const data = [...prevState.data];
-      //   //         data.splice(data.indexOf(oldData), 1);
-      //   //         return { ...prevState, data };
-      //   //       });
-      //   //     }, 600);
-      //   //   }),
+      //   onRowAdd: newData =>
+      //     new Promise(resolve => {
+      //       setTimeout(() => {
+      //         resolve();
+      //         this.setState(prevState => {
+      //           const data = [...prevState.data];
+      //           data.push(newData);
+      //           return { ...prevState, data };
+      //         });
+      //       }, 600);
+      //     }),
+      //   onRowUpdate: (newData, oldData) =>
+      //     new Promise(resolve => {
+      //       setTimeout(() => {
+      //         resolve();
+      //         if (oldData) {
+      //           this.setState(prevState => {
+      //             const data = [...prevState.data];
+      //             data[data.indexOf(oldData)] = newData;
+      //             return { ...prevState, data };
+      //           });
+      //         }
+      //       }, 600);
+      //     }),
+      //   onRowDelete: oldData =>
+      //     new Promise(resolve => {
+      //       setTimeout(() => {
+      //         resolve();
+      //         this.setState(prevState => {
+      //           const data = [...prevState.data];
+      //           data.splice(data.indexOf(oldData), 1);
+      //           return { ...prevState, data };
+      //         });
+      //       }, 600);
+      //     }),
       // }}
     />
+    </Container>
   );
     }
 }
