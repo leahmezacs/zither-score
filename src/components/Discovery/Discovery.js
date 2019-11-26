a9fb8c149bf084789d959e45f33689abe2586a14
Yu Ting Yang <yuting7376@gmail.com>
2019-11-26 15:50:01 -0500

    modify table column name

--- src/components/Discovery/Discovery.js
+++ src/components/Discovery/Discovery.js
@@ -14,20 +14,20 @@
 //     // this.handleEditScore = this.handleEditScore.bind(this);
 //   }
 
-//   //get list of scores from all users
-//   async componentDidMount() {
-//     const limit = 50;
-//     const user = await Auth.currentAuthenticatedUser();
-//     const result = await API.graphql(
-//       graphqlOperation(queries.listScores, {limit})
-//     );
-//     console.log(result);
-//     this.setState({
-//       scores: result.data.listScores.items,
-//       userId: user.username
-//     });
-//     console.log(this.state.scores);
-//   }
+  // //get list of scores from all users
+  // async componentDidMount() {
+  //   const limit = 50;
+  //   const user = await Auth.currentAuthenticatedUser();
+  //   const result = await API.graphql(
+  //     graphqlOperation(queries.listScores, {limit})
+  //   );
+  //   console.log(result);
+  //   this.setState({
+  //     scores: result.data.listScores.items,
+  //     userId: user.username
+  //   });
+  //   console.log(this.state.scores);
+  // }
 
 // //   componentWillUnmount() {
 // //     if (this.scoreDeletionSubscription) {
@@ -152,31 +152,19 @@ const tableIcons = {
   ViewColumn: ViewColumn,
   };
 
-// export default function MaterialTableDemo() {
 class Discovery extends Component {
-  // const [state, setState] = React.useState({
     constructor(props){
       super(props);
 
       this.state = {
         columns: [
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
+          { title: 'Score Name', field: 'scoreName' },
+          { title: 'Author', field: 'author' },
+          { title: 'Modify Date', field: 'modifyDate' },
         ],
         data: [
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          {
-            name: 'Zerya Betül',
-            surname: 'Baran',
-            birthYear: 2017,
-            birthCity: 34,
-          },
+          { scoreName: 'happy', author: 'sad', modifyDate: 1987 },
+          { scoreName: 'test', author: 'sad', modifyDate: 1987 },
         ],
       }
     }
