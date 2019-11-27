2a4263800bf60f3fcc58c4f6c3cfb527d6e53992
Jessica <jchu0398@gmail.com>
2019-11-27 01:49:32 -0500

    renamed view button to view/print

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -182,7 +182,7 @@ class Library extends Component {
                                         <MoreVertIcon />
                                     </Dropdown.Toggle>
                                     <Dropdown.Menu>
-                                        <Dropdown.Item onClick={() => this.handlePreviewScore(score.name, score.id)}>View</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => this.handlePreviewScore(score.name, score.id)}>View/Print</Dropdown.Item>
                                         <Dropdown.Item onClick={() => this.handleEditScore(score.name)}>Edit</Dropdown.Item>
                                         <Dropdown.Item onClick={() => this.handleChangeStatus(score.status, score.name)}>Change status</Dropdown.Item>
                                         <Dropdown.Item onClick={() => this.handleDeleteScore(score.name)}>Delete</Dropdown.Item>
