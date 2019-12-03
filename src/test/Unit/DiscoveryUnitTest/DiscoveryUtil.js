57d8cb2b970acd768771951105391b526268bcd2
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-12-03 12:15:38 -0500

    update the test

---
+++ src/test/Unit/DiscoveryUnitTest/DiscoveryUtil.js
@@ -0,0 +1,43 @@
+export function filterScoreName(value, old) {
+    if (value == "All") {
+        delete old["scoreName"];
+    } else {
+        old["scoreName"] = { "contains": value };
+    }
+    return old;
+};
+
+export function filterAuthor(value, old) {
+    if (value == "All") {
+        delete old["author"];
+    } else {
+        old["author"] = { "contains": value };
+    }
+    return old;
+}
+
+
+export function filterDateGen(value, old) {
+    var moment = require('moment');
+    let diff = value * 24 * 60 * 60 * 1000; //convert to milliseconds
+    let current = new Date();
+    let setTime = current - diff;
+    let setDate = moment(setTime).format('YYYY-MM-DD');
+    old["modifyDate"] = { "ge": setDate };
+    return old;
+}
+
+
+export function searchByNameGen(value, searchType, old) {
+
+    if (value == "") {
+        delete old["scoreName"];
+        delete old["author"];
+    } else if (searchType == "Name") {
+        old["searchFieldName"] = { "contains": value.toLowerCase() };
+    } else {
+        old["searchFieldLocation"] = { "contains": value.toLowerCase() };
+    }
+    return old;
+}
+
