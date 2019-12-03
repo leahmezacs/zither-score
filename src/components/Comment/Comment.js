46a0ed0f54b18ab3c2fe9ff6787e515d534558cf
Yu Ting Yang <yuting7376@gmail.com>
2019-12-03 15:05:26 -0500

    add a form to the comment for posting comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
+import { Container, TextField, Button } from "@material-ui/core";
 
 class Comment extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        comment: '',
+        comment:'',
     };
-    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange = event => {};
+
+  handleSubmit = event => {};
 
   render() {
     return (
-      
+      <Container maxWidth="lg">
+        <form onSubmit={this.handleSubmit}>
+          <TextField
+            fullWidth
+            type="text"
+            rows={5}
+            rowsMax={5}
+            multiline
+            disableunderline="true"
+            inputProps={{ maxLength: 600 }}
+            label="Leave a comment"
+            variant="outlined"
+            onChange={this.handleChange}
+          />
+        </form>
+      </Container>
     );
   }
 }
