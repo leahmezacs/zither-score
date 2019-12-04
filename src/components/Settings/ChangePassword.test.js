dd5be7640650ddc21071464027a9f77c659aa9cf
Jessica <jchu0398@gmail.com>
2019-12-04 00:50:04 -0500

    test: submit button simulated

--- src/components/Settings/ChangePassword.test.js
+++ src/components/Settings/ChangePassword.test.js
@@ -8,3 +8,11 @@ test("expect component is called at least once", () => {
     const wrapper = shallow(<ChangePassword />);
     expect(wrapper.find(".ChangePassword").length).toEqual(1);
 });
+
+it('calls onSubmit prop function when form is submitted', () => {
+    const onSubmitFn = jest.fn();
+    const wrapper = mount(<form onSubmit={onSubmitFn}/>);
+    const form = wrapper.find('form');
+    form.simulate('submit');
+    expect(onSubmitFn).toHaveBeenCalledTimes(1);
+});
