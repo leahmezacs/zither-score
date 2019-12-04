f4ce7eef08061942926c6c39188af061f4300eaa
Mengting Xia <mxia7498@gmail.com>
2019-12-04 00:12:43 -0500

    add users and scores button to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,37 +10,48 @@ import * as queries from '../../graphql/queries';
 import * as mutations from '../../graphql/mutations';
 import * as subscriptions from '../../graphql/subscriptions';
 import ListUsers from './ListUsers';
+import "./Dashboard.css";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      users: true,
+      scores: false
     };
+    this.handleChangeClick = this.handleChangeClick.bind(this);
   }
+
+  handleChangeClick = () => {
+    this.setState(prevState => ({
+      users: !prevState.users,
+      scores: !prevState.scores
+    }));
+  };
 
   render() {
     return (
-      <Container maxWidth="md">
+      <div className="dashboard-root">
         <br />
         <Drawer
           className="drawer"
           variant="permanent"
-          classes={{
-            paper: "drawerPaper",
-          }}
         >
           {/* <div className={classes.toolbar} /> */}
           <List>
-            {['User', 'Scores'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+            <ListItem button onClick={this.handleChangeClick}>
+              <ListItemText primary="Users" />
+            </ListItem>
+            <ListItem button onClick={this.handleChangeClick}>
+              <ListItemText primary="Scores" />
+            </ListItem>
           </List>
         </Drawer>
-        <ListUsers />
-      </Container>
+        <main className="content">
+          { this.state.users === true ? <ListUsers /> : <ListScores /> }
+        </main>
+      </div>
     );
   }
 }
