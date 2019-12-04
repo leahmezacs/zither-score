dc819f696e8841cba459e27ff0110d3d43c98bcf
Jessica <jchu0398@gmail.com>
2019-12-04 00:49:29 -0500

    test: ChangePassword renders

--- src/components/Settings/ChangePassword.test.js
+++ src/components/Settings/ChangePassword.test.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ChangePassword from './ChangePassword';
+import { Button } from 'react-bootstrap';
+import LoaderButton from "./LoaderButton";
+import { shallow, mount } from 'enzyme';
+
+test("expect component is called at least once", () => {
+    const wrapper = shallow(<ChangePassword />);
+    expect(wrapper.find(".ChangePassword").length).toEqual(1);
+});
