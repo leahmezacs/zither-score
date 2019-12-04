7d5b104bd78838fb9651b7bdb6e725a5d2e97e7d
Jessica <jchu0398@gmail.com>
2019-12-04 00:39:40 -0500

    test: div classes render

--- src/components/Settings/Settings.test.js
+++ src/components/Settings/Settings.test.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import Settings from './Settings';
+import { shallow } from 'enzyme';
+
+test("expect component is called at least once", () => {
+    const wrapper = shallow(<Settings />);
+    expect(wrapper.find(".container").length).toEqual(1);
+    expect(wrapper.find(".content").length).toEqual(1);
+});
