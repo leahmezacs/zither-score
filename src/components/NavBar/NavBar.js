b18ada05050fb03c71285381b2677e48d8e65b77
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-12-06 11:25:42 -0500

    add help page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,6 +8,7 @@ import CreateModal from "../CreateModal/CreateModal";
 import { Navbar, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
 import { graphqlOperation, API } from 'aws-amplify';
 import * as queries from '../../graphql/queries';
+import HelpFQA from '../HelpFQA/HelpFQA'
 
 class NavBar extends Component {
   constructor(props) {
@@ -63,7 +64,7 @@ class NavBar extends Component {
                     <NavDropdown.Item href="/Library">Library</NavDropdown.Item>
                     <NavDropdown.Item href="/Settings">Settings</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#">Help</NavDropdown.Item>
+                    <NavDropdown.Item href="/HelpFQA">Help</NavDropdown.Item>
                     <NavDropdown.Item href="/" onClick={this.props.onSignOut}>
                       Logout
                     </NavDropdown.Item>
