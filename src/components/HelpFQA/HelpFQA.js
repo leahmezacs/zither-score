0b312c2b89812ba6364678f86131a5deded02f1b
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-12-10 12:01:15 -0500

    clean up the css classes

--- src/components/HelpFQA/HelpFQA.js
+++ src/components/HelpFQA/HelpFQA.js
@@ -5,27 +5,27 @@ class HelpFQA extends Component {
     render() {
         return (
             <div>
-                <header class="cd-header flex flex-column flex-center">
-                    <div class="text-component text-center">
+                <header>
+                    <div>
                         <h1>NumScore User's FAQ</h1>
                     </div>
                 </header>
-                <div class="cd-faq js-cd-faq container max-width-md margin-top-lg margin-bottom-lg">
-                    <ul class="cd-faq__categories">
-                        <li><a class="cd-faq__category cd-faq__category-selected truncate" href="#basics">Basics</a></li>
-                        <li><a class="cd-faq__category truncate" href="#account">Account</a></li>
+                <div>
+                    <ul>
+                        <li><a href="#basics">Basics</a></li>
+                        <li><a href="#account">Account</a></li>
 
 
                     </ul>
-                    <div class="cd-faq__items">
-                        <ul id="basics" class="cd-faq__group">
+                    <div>
+                        <ul id="basics" >
                             <li class="cd-faq__title">
                                 <h2>Basics</h2>
                             </li>
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>How do I change my password?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>How do I change my password?</span></a>
+                                <div>
+                                    <div>
                                         <p>First, go to NavBar and click on icon, and then click login, the web will direct you to a
                                             login page. Second, click forgot password, the page will direct you enter email address
                                             so and so, you will reveive temporary password, and then you can log in to your account,
@@ -34,10 +34,10 @@ class HelpFQA extends Component {
                                 </div>
                             </li>
 
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>How do I sign up?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>How do I sign up?</span></a>
+                                <div>
+                                    <div>
                                         <p>First, go to NavBar and click on icon, and then click login, if you do not have an
                                             account, there is a button called Create a account, after that you need to filled in
 								every box in order to create an account.</p>
@@ -45,10 +45,10 @@ class HelpFQA extends Component {
                                 </div>
                             </li>
 
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>Can I remove a score sheet?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>Can I remove a score sheet?</span></a>
+                                <div>
+                                    <div>
                                         <p>First, go to NavBar and click on icon, and then click library, you will see the score
                                             sheets you created, and then click on three dots on the right, it will show you a
 								dropdown menue, and then you can delete your score sheet.</p>
@@ -56,10 +56,10 @@ class HelpFQA extends Component {
                                 </div>
                             </li>
 
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>How do reviews work?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>How do reviews work?</span></a>
+                                <div>
+                                    <div>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis,
                                             reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit,
 								magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.</p>
@@ -69,14 +69,14 @@ class HelpFQA extends Component {
                         </ul>
 
 
-                        <ul id="account" class="cd-faq__group">
-                            <li class="cd-faq__title">
+                        <ul id="account">
+                            <li>
                                 <h2>Account</h2>
                             </li>
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>How do I change my password?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>How do I change my password?</span></a>
+                                <div>
+                                    <div>
                                         <p>First, go to NavBar and click on icon, and then click login, the web will direct you to a
                                             login page. Second, click forgot password, the page will direct you enter email address
                                             so and so, you will reveive temporary password, and then you can log in to your account,
@@ -85,29 +85,29 @@ class HelpFQA extends Component {
                                 </div>
                             </li>
 
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>How do I delete my account?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>How do I delete my account?</span></a>
+                                <div>
+                                    <div>
                                         <p>Admin users can delete account on the admin page.</p>
                                     </div>
                                 </div>
                             </li>
 
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>How do I change my account settings?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>How do I change my account settings?</span></a>
+                                <div>
+                                    <div>
                                         <p>First go to nav bar and click on icon, and then it will show you a drop down menu, you
 								can click on settings to change the settings of your account.</p>
                                     </div>
                                 </div>
                             </li>
 
-                            <li class="cd-faq__item">
-                                <a class="cd-faq__trigger" href="#0"><span>I forgot my password. How do I reset it?</span></a>
-                                <div class="cd-faq__content">
-                                    <div class="text-component">
+                            <li>
+                                <a href="#0"><span>I forgot my password. How do I reset it?</span></a>
+                                <div>
+                                    <div>
                                         <p>First, go to NavBar and click on icon, and then click login, the web will direct you to a
                                             login page. Second, click forgot password, the page will direct you enter email address
                                             so and so, you will reveive temporary password, and then you can log in to your account.
@@ -119,9 +119,9 @@ class HelpFQA extends Component {
 
 
 
-                        <a href="#0" class="cd-faq__close-panel text-replace">Close</a>
+                        <a href="#0" >Close</a>
 
-                        <div class="cd-faq__overlay" aria-hidden="true"></div>
+                        <div aria-hidden="true"></div>
                     </div>
                 </div>
                 <script src="util.js"></script>
