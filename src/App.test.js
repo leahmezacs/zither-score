eb954a19f575e2f08f77be83413079e1dbe5ac98
Jessica <jchu0398@gmail.com>
2019-12-11 01:33:15 -0500

    Test: App.test e2e

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,32 @@ import React from "react";
 import { shallow, mount, render } from "enzyme";
 import App from "./App";
 
+const puppeteer = require('puppeteer')
+
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+}
+
+describe('on page load', () => {
+  test('h1 loads correctly', async() => {
+    let browser = await puppeteer.launch({});
+    let page = await browser.newPage();
+    
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: ''
+    })
+  })
+})
+
 describe("App", () => {
     it("Renders without crashing", () => {
         const app = shallow(<App />);
