155a37f25e1f5f9db341bb78b991f2b2878b7754
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-12-13 15:33:22 -0500

    modify

--- src/test/Unit/Reply.test.js
+++ src/test/Unit/Reply.test.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import {render, cleanup, fireEvent} from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import CreateModal from '../../components/Comment/Reply'
 
 afterEach(cleanup)
 
-test('on submit comments', () =>{
+test('on submit comments', () => {
     const handleSubmit = jest.fn()
-    
 
-    const {getByLabelText,getByText,getByDisplayValue} = render(
+
+    const { getByLabelText, getByText, getByDisplayValue } = render(
         <Reply
-          submit={handleSubmit}
-          Change ={handleChange}
-          openReplyForm = {openReplyForm}
-          closeReplyForm = {closeReplyForm}
-          
+            submit={handleSubmit}
+            Change={handleChange}
+            openReplyForm={openReplyForm}
+            closeReplyForm={closeReplyForm}
+
         />,
     )
-    
+
 
 
     expect(handleSubmit).toHaveBeenCalledTimes(1)
