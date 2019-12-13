3e4001ad7c5d0a8aba849e07e5059d5348d116bc
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-12-13 10:57:23 -0500

    left notes for developers

--- src/components/EditScore/SingleScoreInput.js
+++ src/components/EditScore/SingleScoreInput.js
@@ -442,3 +442,6 @@ export default SingleScoreInput;
 you can add more later, just notes that the single line and double
 lines can only be the bottom, but the dot and double dots can be 
 both top and bottom,  however it should not appear at the same time.*/}
+
+{/*some symbols can not be find, but once it figure out we can add it to
+the selections*/}
