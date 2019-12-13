0fa71f71830d3d85aa567208e0e03bc166d47d39
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-12-13 11:26:23 -0500

    add test for help page

---
+++ src/test/Unit/HelpFAQ.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+import NavBar from '../../components/HelpFAQ';
+import HelpFQA from '../../components/HelpFAQ/HelpFAQ';
+
+test('help renders guest view', () => {
+  const { getByTestId } = render(
+    <MemoryRouter>
+      <HelpFQA />
+    </MemoryRouter>,
+  );
+  expect(getByTestId('help')).toHaveTextContent('help');
+});
