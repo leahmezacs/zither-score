00750ea42c1f6275e821021149820c29209a79be
Yu Ting Yang <yuting7376@gmail.com>
2019-12-16 19:28:39 -0500

    clean up code for navBar.js file

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 import { withRouter, Redirect } from "react-router-dom";
-import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusicOutlined";
 import "../../stylesheets/style.css";
 import CreateModal from "../CreateModal/CreateModal";
-import { Navbar, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
-import { graphqlOperation, API } from 'aws-amplify';
-import * as queries from '../../graphql/queries';
-
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 class NavBar extends Component {
   constructor(props) {
@@ -33,7 +29,7 @@ class NavBar extends Component {
       return <Redirect to="/" />;
     }
     console.log(this.props.loggedInUser);
-    
+
     return (
       <div>
         <Navbar className="NavBarBackground" expand="lg">
@@ -42,55 +38,71 @@ class NavBar extends Component {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-              <Nav.Link className="text-light link font-weight-bold" href="/Discovery">
+            <Nav className="mr-auto">
+              <Nav.Link
+                className="text-light link font-weight-bold"
+                href="/Discovery"
+              >
                 Discover
               </Nav.Link>
-              <Nav.Link className="text-light link font-weight-bold" href="/HelpFAQ">
+              <Nav.Link
+                className="text-light link font-weight-bold"
+                href="/HelpFAQ"
+              >
                 Help
               </Nav.Link>
             </Nav>
-            
-            { this.props.loggedInUser ? (          
-               this.props.loggedInUser.signInUserSession.accessToken.payload['cognito:groups'] == "User" ? (
+
+            {this.props.loggedInUser ? (
+              this.props.loggedInUser.signInUserSession.accessToken.payload[
+                "cognito:groups"
+              ] == "User" ? (
                 <>
                   <Nav className="ml-auto">
-                    <Nav.Link className="link font-weight-bold" onClick={this.handleShow}>Create</Nav.Link>
+                    <Nav.Link
+                      className="link font-weight-bold"
+                      onClick={this.handleShow}
+                    >
+                      Create
+                    </Nav.Link>
                   </Nav>
 
-                  <NavDropdown 
+                  <NavDropdown
                     title={<AccountCircle fontSize="large" color="disabled" />}
                     id="basic-nav-dropdown"
                     className="float-right"
                   >
-                    {/* <NavDropdown.Item href="#">Profile</NavDropdown.Item> */}
                     <NavDropdown.Item href="/Library">Library</NavDropdown.Item>
-                    <NavDropdown.Item href="/Settings">Settings</NavDropdown.Item>
+                    <NavDropdown.Item href="/Settings">
+                      Settings
+                    </NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/" onClick={this.props.onSignOut}>
                       Logout
                     </NavDropdown.Item>
                   </NavDropdown>
                 </>
-            ) : /* this.props.loggedInUser.signInUserSession.accessToken.payload['cognito:groups'] == "Admin" ? */ (
+              ) : (
                 <>
-                  <NavDropdown 
+                  <NavDropdown
                     title={<AccountCircle fontSize="large" color="disabled" />}
                     id="basic-nav-dropdown"
                     className="float-right"
                   >
-                    <NavDropdown.Item href="/Dashboard">Dashboard</NavDropdown.Item>
+                    <NavDropdown.Item href="/Dashboard">
+                      Dashboard
+                    </NavDropdown.Item>
                     <NavDropdown.Item href="/" onClick={this.props.onSignOut}>
                       Logout
                     </NavDropdown.Item>
                   </NavDropdown>
                 </>
-            /* ) : (
-              <Nav.Link className="text-light font-weight-bold" href="/Login">
-                Login
-              </Nav.Link> */
-            )) : (
-              <Nav.Link className="text-light link font-weight-bold" href="/Login">
+              )
+            ) : (
+              <Nav.Link
+                className="text-light link font-weight-bold"
+                href="/Login"
+              >
                 Login
               </Nav.Link>
             )}
